cc7dcb63b1161e01f94c8f9b780bbb6db4ca44e7
JayZonday <jmoloughney25@gmail.com>
2019-11-19 00:48:40 -0500

    first user seed test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,49 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+
+  def create
+    @user = User.create(username: params["username"], password: params["password"], motto: params["motto"],  name: params["name"], hobby: params['hobby'], profilepic: params['profilepic'], dogname: params['dogname'])
+    if (@user && @user.authenticate(params["password"]))
+      render json: token_json(@user)
+    else
+      render json: {
+        errors: "Those credentials are invalid"
+      }, status: :unauthorized
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+
+  def destroy
+    User.destroy(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :username,
+      :password,
+      :email,
+      :name,
+      :hobby,
+      :profilepic,
+      :dogname
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(
+  username: 'Test',
+  password:"test",
+  motto: "I Test Non-Stop!",
+  name:"Test Tester",
+  hobby:"Testing",
+  profilepic:"https://www.robot-advance.com/EN/ori-maze-breaker-black-silverlit-1885.jpg",
+  dogname:"Buster")
